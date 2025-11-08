46d4ea7ebfa6d48ba25fd93cb7ab8993f01c0c24
Jon Craton <jncraton@gmail.com>
2025-11-08 15:55:09 -0500

    Test multiple paragraphs

--- test_index.py
+++ test_index.py
@@ -17,3 +17,8 @@ def test_page_title(root):
 def test_paragraph(root):
     root.locator("textarea").fill("Hello world")
     expect(root.locator("p")).to_have_text("Hello world")
+
+def test_paragraphs(root):
+    root.locator("textarea").fill("p1\n\np2")
+    expect(root.locator("p").first).to_have_text("p1")
+    expect(root.locator("p").last).to_have_text("p2")
