6de1eefbf322cf728fe4f99536f84208e896ba52
Jon Craton <jncraton@gmail.com>
2025-11-08 16:27:33 -0500

    Add element creation helper function

--- ugfm.js
+++ ugfm.js
@@ -1,5 +1,11 @@
 const ugfm = markdown => {
-  const article = document.createElement('article')
+  const el = (name, text) => {
+    const newElement = document.createElement(name)
+    newElement.textContent = text
+    return newElement
+  }
+
+  const article = el('article')
   const blocks = markdown.split(/\n\n+/)
 
   blocks.forEach(text => {
@@ -7,26 +13,20 @@ const ugfm = markdown => {
     const cleanedText = text.replace(/^#+/, '')
     const headingLevel = text.length - cleanedText.length
 
-    let el
 
     if (headingLevel) {
-      el = document.createElement(`h${headingLevel}`)
-      el.textContent = cleanedText
+      article.append(el(`h${headingLevel}`, cleanedText))
     } else if (text[0] == '-') {
-      el = document.createElement('ul')
+      ul = el('ul')
       text.split(/^\- /gm).forEach(item => {
         if (item.trim()) {
-          const li = document.createElement('li')
-          li.textContent = item
-          el.append(li)
+          ul.append(el('li', item))
         }
       })
+      article.append(ul)
     } else {
-      el = document.createElement('p')
-      el.textContent = text
+      article.append(el('p', text))
     }
-
-    article.append(el)
   })
 
   return article
