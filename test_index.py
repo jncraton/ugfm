328074c16239b6d4c9692c9b53c5b4cb392f6b34
Jon Craton <jncraton@gmail.com>
2025-11-08 14:10:27 -0500

    Add test_index.py

---
+++ test_index.py
@@ -0,0 +1,15 @@
+import re
+from playwright.sync_api import Page, expect
+import pytest
+
+from pathlib import Path
+
+file_url = Path("index.html").resolve().as_uri()
+
+@pytest.fixture
+def root(page: Page):
+    page.goto(f"{file_url}#")
+    return page
+
+def test_page_title(root):
+    expect(root).to_have_title("ugfm")
